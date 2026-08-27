324715b145298f9b20dded67c00329d4fe5faa9c
Bisham Neupane <bishamneupane800@gmail.com>
2026-08-27 18:12:02 +0545

    Update project changes

--- babylon-school-backend/src/app.js
+++ babylon-school-backend/src/app.js
@@ -28,7 +28,7 @@ const posterRoutes = require("./routes/poster.routes");
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
